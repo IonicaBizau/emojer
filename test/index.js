6a547321c96c87be401d52b30022dea427001129
Ionică Bizău <bizauionica@gmail.com>
2017-11-09 20:34:05 +0200

    Fix typo and add tests

--- test/index.js
+++ test/index.js
@@ -6,4 +6,7 @@ tester.describe("emoji", test => {
     test.it("convert the emoji from input string", () => {
         test.expect(emojer("Hello :world:", "{__EMOJI_NAME__}")).toBe("Hello {world}");
     });
+    test.should("not convert :everything:", () => {
+        test.expect(emojer("Hello :world: and not :this:", "{__EMOJI_NAME__}", { world: true })).toBe("Hello {world} and not :this:");
+    });
 });
